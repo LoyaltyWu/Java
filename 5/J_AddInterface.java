e3e178e3b54ad9907b8b0485962b4d7c5ae39c8a
Wu Rong Hua <LoyaltyWu@users.noreply.github.com>
2016-08-09 16:52:46 +0800

    Add files via upload

---
+++ 5/J_AddInterface.java
@@ -0,0 +1,18 @@
+// ////////////////////////////////////////////
+//
+// J_AddInterface.java
+// 
+// 开发者:LoyaltyWu
+// ////////////////////////////////////////////
+// 简介：
+// 			泛型例程
+// ////////////////////////////////////////////
+// 定义了一个J_AddInterface类
+public class J_AddInterface 
+{
+	public static void main (String args[])
+	{
+		System.out.println("Java,Hello!");																					//注意到此函数是println,也就是print a line 
+		//System.out.print("(o゜▽゜)o☆[BINGO!]");																			//用print就不会换行了
+	} // 方法 main 结束
+} //类 J_AddInterface 结束
